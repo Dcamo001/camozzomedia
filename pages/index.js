f94bba6b5227529050607d7f568dc0bcb46ceffb
Daniel Camozzo <dcamo001@gmail.com>
2018-08-29 11:03:32 -0700

    add poopy thumbnail example

--- pages/index.js
+++ pages/index.js
@@ -13,20 +13,33 @@ export default class Index extends Component {
     data.shift(); // remove "images/", which is the first item in the array
     data.map(obj => {
       if (!obj.Key.endsWith(".jpg")) {
-        const title = obj.Key.replace("images/", "").replace("/", "");
-        const gallery = {
+        // if does not end with .jpg, therefore a folder.
+        const title = obj.Key.replace("images/", "").replace("/", ""); // strip unnecessary string data
+        let gallery = {
           title
         };
 
         const images = [];
+        let thumbnail = "";
+        let addedThumbnail = false;
 
-        data.filter(function({ Key }) {
+        data.filter(function({ Key }, i) {
+          // get all images related to that category
           if (Key.includes(title) && Key.includes(".jpg")) {
             images.push(Key);
+            if (!addedThumbnail) {
+              thumbnail = Key;
+              addedThumbnail = true;
+            }
           }
         });
 
-        gallery.images = images;
+        gallery = {
+          ...gallery,
+          thumbnail,
+          images
+        };
+
         galleries.push(gallery);
       }
     });
@@ -34,18 +47,24 @@ export default class Index extends Component {
     this.setState({ galleries });
   }
 
-  getImage = async () => {};
-
   render() {
+    console.log(this.state.galleries);
     return (
       <div>
         <p>Hello Next.js</p>
-        {this.state.galleries.map(({ title, images }, i) => (
+        {this.state.galleries.map((gallery, i) => (
           <div className="gallery" key={i}>
-            <h2>{title}</h2>
+            <h2>{gallery.title}</h2>
+            <img
+              src={`https://s3-us-west-1.amazonaws.com/camozzomedia/${
+                gallery.thumbnail
+              }`}
+              key={i}
+            />
             <div className="images">
-              {images.map((img, i) => (
+              {gallery.images.map((img, i) => (
                 <img
+                  style={{ width: 200 }}
                   key={i}
                   src={`https://s3-us-west-1.amazonaws.com/camozzomedia/${img}`}
                 />
